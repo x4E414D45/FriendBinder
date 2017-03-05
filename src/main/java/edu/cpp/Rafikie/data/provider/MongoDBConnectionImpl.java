d79ff8ba322ba916265fccd4e6a0912248f8098e
dat <datdo@rocketmail.com>
2017-03-05 14:16:37 -0800

    Only create MongoClient once

--- src/main/java/edu/cpp/Rafikie/data/provider/MongoDBConnectionImpl.java
+++ src/main/java/edu/cpp/Rafikie/data/provider/MongoDBConnectionImpl.java
@@ -1,7 +1,5 @@
 package edu.cpp.Rafikie.data.provider;
 
-import java.net.UnknownHostException;
-
 import org.springframework.stereotype.Service;
 
 import com.mongodb.DB;
@@ -11,53 +9,53 @@ import com.mongodb.MongoException;
 
 @Service("mongoDBConnection")
 public class MongoDBConnectionImpl implements MongoDBConnection {
-		
-	public	DBCollection createConnection()
-		{
-			try {
 
-				/**** Connect to MongoDB ****/
-				// Since 2.10.0, uses MongoClient
-				MongoClient mongo = new MongoClient("localhost", 27017);
+	private static MongoClient mongo;
 
-				/**** Get database ****/
-				// if database doesn't exists, MongoDB will create it for you
-				DB db = mongo.getDB("testdb");
-
-				/**** Get collection / table from 'testdb' ****/
-				// if collection doesn't exists, MongoDB will create it for you
-				DBCollection table = db.getCollection("users");
-				return table;
-
-
-			} catch (UnknownHostException e) {
-				e.printStackTrace();
-			} catch (MongoException e) {
-				e.printStackTrace();
-			}
-			return null;
-
-		}
-	public	DBCollection createConnectionforUserTable()
-	{
+	public MongoDBConnectionImpl() {
 		try {
+			mongo = new MongoClient("localhost", 27017);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-			/**** Connect to MongoDB ****/
-			// Since 2.10.0, uses MongoClient
-			MongoClient mongo = new MongoClient("localhost", 27017);
-
-			/**** Get database ****/
+	public DBCollection createConnection() {
+		try {
+			/**
+			 * ** Get database ***
+			 */
 			// if database doesn't exists, MongoDB will create it for you
 			DB db = mongo.getDB("testdb");
 
-			/**** Get collection / table from 'testdb' ****/
+			/**
+			 * ** Get collection / table from 'testdb' ***
+			 */
+			// if collection doesn't exists, MongoDB will create it for you
+			DBCollection table = db.getCollection("users");
+			return table;
+		} catch (MongoException e) {
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+
+	public DBCollection createConnectionforUserTable() {
+		try {
+			/**
+			 * ** Get database ***
+			 */
+			// if database doesn't exists, MongoDB will create it for you
+			DB db = mongo.getDB("testdb");
+
+			/**
+			 * ** Get collection / table from 'testdb' ***
+			 */
 			// if collection doesn't exists, MongoDB will create it for you
 			DBCollection table = db.getCollection("userDetails");
 			return table;
 
-
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
 		} catch (MongoException e) {
 			e.printStackTrace();
 		}
@@ -65,58 +63,46 @@ public class MongoDBConnectionImpl implements MongoDBConnection {
 
 	}
 
-	public	DBCollection createConnectionforUserImageTable()
-	{
+	public DBCollection createConnectionforUserImageTable() {
 		try {
-
-			/**** Connect to MongoDB ****/
-			// Since 2.10.0, uses MongoClient
-			MongoClient mongo = new MongoClient("localhost", 27017);
-
-			/**** Get database ****/
+			/**
+			 * ** Get database ***
+			 */
 			// if database doesn't exists, MongoDB will create it for you
 			DB db = mongo.getDB("testdb");
 
-			/**** Get collection / table from 'testdb' ****/
+			/**
+			 * ** Get collection / table from 'testdb' ***
+			 */
 			// if collection doesn't exists, MongoDB will create it for you
 			DBCollection table = db.getCollection("userImage");
 			return table;
-
-
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
 		} catch (MongoException e) {
 			e.printStackTrace();
 		}
 		return null;
 
 	}
-	
-	public	DBCollection createConnectionforUserNotificationTable()
-	{
+
+	public DBCollection createConnectionforUserNotificationTable() {
 		try {
-
-			/**** Connect to MongoDB ****/
-			// Since 2.10.0, uses MongoClient
-			MongoClient mongo = new MongoClient("localhost", 27017);
-
-			/**** Get database ****/
+			/**
+			 * ** Get database ***
+			 */
 			// if database doesn't exists, MongoDB will create it for you
 			DB db = mongo.getDB("testdb");
 
-			/**** Get collection / table from 'testdb' ****/
+			/**
+			 * ** Get collection / table from 'testdb' ***
+			 */
 			// if collection doesn't exists, MongoDB will create it for you
 			DBCollection table = db.getCollection("notifications");
 			return table;
-
-
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
 		} catch (MongoException e) {
 			e.printStackTrace();
 		}
 		return null;
 
 	}
-		
-		}
+
+}
